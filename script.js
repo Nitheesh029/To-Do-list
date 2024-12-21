a7240117ba1b239677fe7a1f8a60bef5b181ab80
S Nitheesh <qxnitheesh@gmail.com>
2024-12-21 14:26:03 +0530

    Creating the remove task button interactive

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ function getTask(){
 
     inputBox.value = "";
 }
+
+listContainer.addEventListener('click', (e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
