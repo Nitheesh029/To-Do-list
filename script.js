e1fcedbc9d576ad3ec6ee2efcd67b1e8b2660181
S Nitheesh <qxnitheesh@gmail.com>
2024-12-21 14:31:46 +0530

    Storing the data in local storage

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-
 function getTask(){
     if(inputBox.value === ''){
         alert("You must write something!");
@@ -14,12 +13,25 @@ function getTask(){
     }
 
     inputBox.value = "";
+    saveData();
 }
 
 listContainer.addEventListener('click', (e)=>{
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
+    
 },false);
+
+function saveData(){
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showData(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showData();
